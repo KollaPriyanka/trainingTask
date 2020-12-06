119864b38434e9313785ee8a8217f9a4e953f6f3
SumaVarshitha <sumavarshitha.kamatam997@gmail.com>
2020-12-06 20:25:32 +0530

    Update dockerbuild.sh

--- Dockershellscript/dockerbuild.sh
+++ Dockershellscript/dockerbuild.sh
@@ -1,5 +1,5 @@
 mkdir Training
 cp Dockerfile ./Training/Dockerfile
 cd Training
-docker build -f Dockerfile -t sample:1 .
-docker push sample:1
+docker build -f Dockerfile -t sumavarshitha/sample:1 .
+docker push sumavarshitha/sample:1
